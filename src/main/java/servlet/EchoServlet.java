45f694660481541ad6d33c47110b3d35fd707107
ozairi121 <ozairi121@gmail.com>
2020-04-09 13:01:13 -0400

    convert params to json

--- src/main/java/servlet/EchoServlet.java
+++ src/main/java/servlet/EchoServlet.java
@@ -32,37 +32,24 @@ public class EchoServlet extends HttpServlet
      String para;
      Enumeration paraNames = req.getParameterNames();
 
-     StringBuilder responseStr = new StringBuilder("");
+     StringBuilder responseStr = new StringBuilder("{");
 
      while (paraNames.hasMoreElements())
      {  // For each parameter name.
        para = (String)paraNames.nextElement();
        if (!para.equalsIgnoreCase("submit"))
        {
-         responseStr.append("  <tr>");
-         responseStr.append("    <td style=\"width: 20%\" width=\"20%\"><b>" + para + "</b></td>");
+         responseStr.append("\"" + para + "\"");
 
          String[] values = req.getParameterValues(para);
 
          if (values != null && !values[0].equals(""))
-           responseStr.append("    <td>" + values[0] + "</td></tr>");
+           responseStr.append(":\"" + values[0] + "\",");
          else
-           responseStr.append("    <td>&nbsp;</td></tr>");
-
-         for (int i = 1; i < values.length; i++)
-         {
-           if (!values[i].equals(""))
-           {
-             responseStr.append("  <tr>");
-             responseStr.append("    <td style=\"width: 20%\" width=\"20%\">&nbsp;</td>");
-             responseStr.append("    <td>" + values[i] + "</td></tr>");
-           }
-         }
+           responseStr.append(":null,");
        }
      }
-     responseStr.append("</table>");
-
-     req.setAttribute("params", responseStr); // Will be available as ${params} in JSP
+     responseStr.append("}");
 
      pw.println(responseStr);
      pw.close();
